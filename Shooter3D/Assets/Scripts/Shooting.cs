061f12e039686e01f8877c0e0014796aab4b6258
YeliseyPolonsky <121894577+YeliseyPolonsky@users.noreply.github.com>
2025-03-19 18:07:40 +0300

    shooting

---
+++ Shooter3D/Assets/Scripts/Shooting.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float fireRate = 0.1f;
+    [SerializeField] private GameObject flash;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject bulletMark;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private AudioSource bulletSound;
+
+    private float timer;
+    private Coroutine shootProcess;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetMouseButton(0) && timer >= fireRate)
+        {
+            timer = 0;
+            Shot();
+        }
+    }
+
+    private void Shot()
+    {
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000f, layerMask))
+        {
+            GameObject newBulletMark = Instantiate(bulletMark, hit.point, Quaternion.LookRotation(hit.normal));
+            newBulletMark.transform.parent = hit.transform;
+        }
+        
+        animator.SetTrigger("Shot");
+        bulletSound.Stop();
+        bulletSound.Play();
+
+        if (shootProcess != null)
+        {
+            StopCoroutine(shootProcess);
+        }
+        shootProcess = StartCoroutine(ShootProcess());
+    }
+
+    private IEnumerator ShootProcess()
+    {
+        flash.transform.localScale = Vector3.one * Random.Range(0.8f, 1.2f);
+        flash.transform.localEulerAngles = new Vector3(0, 0, Random.Range(0, 360));
+        flash.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        flash.SetActive(false);
+    }
+}
